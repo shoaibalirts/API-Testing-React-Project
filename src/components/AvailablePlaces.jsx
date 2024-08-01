b2d350e1da9d54cea99d1172d71881008e1477ae
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-01 18:12:12 +0200

    managing isLoading state

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const BASE_API_URL = "http://localhost:3000/";
   const url = new URL("places", BASE_API_URL);
   // console.log(url.toString());
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch(url);
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false); // not fetching the data anymore
     }
     fetchPlaces();
   }, []);
@@ -20,6 +23,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
