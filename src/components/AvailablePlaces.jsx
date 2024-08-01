7e7c003b211f1f2a47963e6e985a29cc1e9348ef
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-01 18:39:25 +0200

    error handling using try catch

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,24 +1,42 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
-
+import Error from "./Error.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
   const BASE_API_URL = "http://localhost:3000/";
   const url = new URL("places", BASE_API_URL);
   // console.log(url.toString());
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch(url);
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch(url);
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed to fetch places.");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        // define the code when the wrror is encountered
+        setError({
+          message:
+            error.message || "Could not fetch places , please try again later",
+        });
+      }
+
       setIsFetching(false); // not fetching the data anymore
     }
     fetchPlaces();
   }, []);
 
-  console.log(availablePlaces);
+  // console.log(availablePlaces);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
