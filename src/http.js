cd24d50d5fdfbc8322a376a3201fcc4d8bc067fb
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-01 20:56:43 +0200

    sent data using Put method

--- src/http.js
+++ src/http.js
@@ -8,3 +8,20 @@ export async function fetchAvailablePlaces() {
   }
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const BASE_API_URL = "http://localhost:3000/";
+  const url = new URL("user-places", BASE_API_URL);
+  const response = await fetch(url, {
+    method: "PUT",
+    body: JSON.stringify({ places: places }), // it should be sent
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to update user data.");
+  }
+  return resData.message;
+}
