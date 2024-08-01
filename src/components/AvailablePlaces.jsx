95fff94fa26e46a984766fad45e0547f201c1d5f
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-01 18:51:57 +0200

    added navigator and transformed fetched data. loading state code is shifted lexically inside navigator callback function

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 import Error from "./Error.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -17,16 +18,23 @@ export default function AvailablePlaces({ onSelectPlace }) {
         if (!response.ok) {
           throw new Error("Failed to fetch places.");
         }
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false); // not fetching the data anymore
+        });
       } catch (error) {
         // define the code when the wrror is encountered
         setError({
           message:
             error.message || "Could not fetch places , please try again later",
         });
+        setIsFetching(false); // not fetching the data anymore
       }
-
-      setIsFetching(false); // not fetching the data anymore
     }
     fetchPlaces();
   }, []);
